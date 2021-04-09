e7e87d063d4f91642e6104d6b1448e8f364c36cf
Gabriel Mateus <gmsf19@gmail.com>
2021-04-09 10:23:09 -0300

    creating structure and styling header

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+  <>
+    <Head>
+      <title>Code | News</title>
+    </Head>
+    <h1>
+      Hello World
+    </h1>
+  </>
+   
+  )
+}
