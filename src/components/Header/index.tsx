ba71a1d9a6e76f7bd6023a77d72ff4398ae5c6a7
Gabriel Mateus <gmsf19@gmail.com>
2021-05-06 20:13:44 -0300

    to clear up doubt

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,21 @@
-import { SignInButton } from "../SignInButton";
-import Link from 'next/link';
-import styles from "./styles.module.scss";
+import { SignInButton } from '../SignInButton';
+import styles from './styles.module.scss';
+import { ActiveLink } from 'components/ActiveLink';
 
 export function Header() {
+
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="code.news" />
         <nav>
-          <Link href='/'> 
-           <a className={styles.active}>Home</a>
-          </Link>
-          <Link href='/posts'>
+          <ActiveLink  activeClassName={styles.active} href="/"> 
+           <a>Home</a> 
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
            <a>Posts</a>
-          </Link>
+          </ActiveLink>
 
         </nav>
 
